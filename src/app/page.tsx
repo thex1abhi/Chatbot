7a32b233ef210985eed3838c2418f77f69587b8b
Abhishek Yadav <yabhishekk480@gmail.com>
2026-03-30 23:53:27 +0530

    Designed nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HomeClient from "@/components/HomeClient";
 
 export default function Home() {
   return (
-     <div className="text-3xl font-semibold ">Home page </div>
+    <>
+     <HomeClient />
+    </>
   );
 }
